771f9e86c4c9c3303d53d5bee914d066bfd93f0b
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2018-11-14 20:02:34 +0100

    Subtrees!
    Not complete yet, but they work, at least.

--- epan/dissectors/packet-syncthing.c
+++ epan/dissectors/packet-syncthing.c
@@ -18,50 +18,38 @@
 
 #define SYNCTHING_LOCAL_DISCOVERY_PORT 21027
 
-typedef struct {
+typedef const struct {
     tvbuff_t *tvb;
     packet_info *pinfo;
     proto_tree *tree;
-    
-    int machine_id_count;
-    int instance_id_count;
-    int addresses_count;
 } syncthing_local_discovery_summary;
 // We all live in a yellow summary!
 
-typedef gint (*syncthing_protobuf_field_handler)(syncthing_local_discovery_summary *summary, guint offset);
+typedef gint (*syncthing_protobuf_field_handler)(syncthing_local_discovery_summary *summary, proto_item *header, guint offset);
 
-typedef struct {
+typedef const struct {
     int tag;
     int wire_type;
     syncthing_protobuf_field_handler handler;
+    int ett;
+    char *label;
 } syncthing_protobuf_field_definition;
 
 
-/* Internal functions */
-// TODO: start_offset and offset are poorly namedTODO
-static gint dissect_next_field(syncthing_local_discovery_summary *summary, guint offset);
-static gint dissect_protobuf_field(
-    syncthing_protobuf_field_definition *definitions,
-    int defcount,
-    syncthing_local_discovery_summary *summary,
-    guint offset
-);
-static gint dissect_machine_id(syncthing_local_discovery_summary *summary, guint offset);
-static gint dissect_address(syncthing_local_discovery_summary *summary, guint offset);
-static gint dissect_instance_id(syncthing_local_discovery_summary *summary, guint offset);
-
 /* Protocols */
+/* Yeah ok, there's only one right now, but syncthing has 4 of them, so maybe later */
 void proto_register_syncthing(void);
-
 static int proto_syncthing_local_discovery = -1;
 
+
 /* Fields */
+static int hf_syncthing_protobuf_entry = -1;
 static int hf_syncthing_protobuf_key = -1;
 static int hf_syncthing_local_magic = -1;
 static int hf_syncthing_local_machine_id = -1;
 static int hf_syncthing_local_address = -1;
 static int hf_syncthing_local_instance_id = -1;
+
 
 /* Trees */
 static gint ett_syncthing_local = -1;
@@ -69,129 +57,20 @@ static gint ett_syncthing_local_machine_id = -1;
 static gint ett_syncthing_local_address = -1;
 static gint ett_syncthing_local_instance_id = -1;
 
+
 /* Expert infos */
 // TODO: static expert_field ei_syncthing_local_malformed = EI_INIT;
 
 
-static int
-dissect_syncthing_local_discovery(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
-{
-    g_print("Dissecting a new packet\n");
-
-    // The first four bytes are 0x2EA7D90B in networ.k (big endian) byte order.
-    if (tvb_bytes_exist(tvb, 0, 4)) {
-        guint32 magic = tvb_get_ntohl(tvb, 0);
-    
-        if (magic == 0x2EA7D90B) {
-            // Ok, this looks like a syncthing local discovery packet
-            col_set_str(pinfo->cinfo, COL_PROTOCOL, "Syncthing");
-            col_clear(pinfo->cinfo, COL_INFO);
-
-
-            proto_item *ti = proto_tree_add_item(tree, proto_syncthing_local_discovery, tvb, 0,-1, ENC_NA);
-            proto_tree *syncthing_tree = proto_item_add_subtree(ti, ett_syncthing_local);
-            proto_tree_add_item(syncthing_tree, hf_syncthing_local_magic, tvb, 0, 4, ENC_BIG_ENDIAN);
-
-            guint offset = 4;
-            syncthing_local_discovery_summary summary = { tvb, pinfo, tree, 0, 0, 0 };
-
-            while (offset < tvb_reported_length(tvb)) {
-                gint data_used = dissect_next_field(&summary, offset);
-
-                g_print("data_used = %i\n", data_used);
-
-                if (data_used != -1) {
-                    offset += data_used;
-                }
-                else {
-                    // TODO: Handle this error
-                    return offset;
-                }
-            }
-
-            return tvb_captured_length(tvb);
-        }
-    }
-
-    return 0;
-}
-
-static gint
-dissect_next_field(syncthing_local_discovery_summary *summary, guint offset)
-{
-    g_print("Dissecting next field\n");
-
-    /*
-    * The format is defined as follows:
-    * message Announce {
-    *   bytes           id          = 1;
-    *   repeated string addresses   = 2;
-    *   int64           instance_id = 3;
-    * }
-    */
-
-    static syncthing_protobuf_field_definition field_definitions[] = {
-        { 1, 2, &dissect_machine_id },
-        { 2, 2, &dissect_address },
-        { 3, 0, &dissect_instance_id }
-    };
-
-    return dissect_protobuf_field(field_definitions, sizeof(field_definitions), summary, offset);
-}
-
-static gint
-dissect_protobuf_field(
-    syncthing_protobuf_field_definition *definitions,
-    int defcount,
-    syncthing_local_discovery_summary *summary,
-    guint offset)
-{
-    gint varint_length;
-    guint64 key;
-	
-    varint_length = tvb_get_varint(summary->tvb, offset, 4, &key, ENC_VARINT_PROTOBUF);
-    if (varint_length != 0) {
-        offset += varint_length;
-
-        const int tag = key >> 3;
-        int wire_type = key & 0x07;
-
-        g_print("wire_type: %i\n", wire_type);
-        g_print("tag: %i\n", tag);
-
-        for (int i = 0; i < defcount; ++i) {
-            syncthing_protobuf_field_definition *def = &definitions[i];
-
-            if (tag == def->tag) {
-                if (wire_type == def->wire_type) {
-                    gint result = def->handler(summary, offset);
-                    return varint_length + result;
-                }
-                else {
-                    // TODO: Add expert info
-                    return -1;
-                }
-            }
-        }
-    }
-
-    // TODO: Add expert info
-    g_print("Invalid varint at beginning of field\n");
-    return -1;
-}
-
 gint
-dissect_machine_id(syncthing_local_discovery_summary *summary, guint offset)
+dissect_machine_id(syncthing_local_discovery_summary *summary, proto_item *header _U_, guint offset)
 {
-    g_print("Dissecting machine id\n");
-    
     guint varint_length;
     guint64 field_length;
 
     varint_length = tvb_get_varint(summary->tvb, offset, 4, &field_length, ENC_VARINT_PROTOBUF);
     if (varint_length != 0) {
         offset += varint_length;
-        summary->machine_id_count += 1;
 
         const guint8 *buf = tvb_get_ptr(summary->tvb, offset, field_length);
 
@@ -212,17 +91,14 @@ dissect_machine_id(syncthing_local_discovery_summary *summary, guint offset)
 }
 
 gint
-dissect_address(syncthing_local_discovery_summary *summary, guint offset)
+dissect_address(syncthing_local_discovery_summary *summary, proto_item *header _U_, guint offset)
 {
-    g_print("Dissecting address\n");
-
     guint varint_length;
     guint64 field_length;
 
     varint_length = tvb_get_varint(summary->tvb, offset, 4, &field_length, ENC_VARINT_PROTOBUF);
     if (varint_length != 0) {
         offset += varint_length;
-        summary->addresses_count += 1;
 
         // TODO: I should free this at some point
         guint8 *buf = (guint8*) wmem_alloc(wmem_packet_scope(), field_length + 1);
@@ -238,10 +114,8 @@ dissect_address(syncthing_local_discovery_summary *summary, guint offset)
 }
 
 gint
-dissect_instance_id(syncthing_local_discovery_summary *summary, guint offset)
+dissect_instance_id(syncthing_local_discovery_summary *summary, proto_item *header _U_, guint offset)
 {
-    g_print("Dissecting instance_id\n");
-
     gint varint_length;
     gint64 instance_id;
 
@@ -249,7 +123,6 @@ dissect_instance_id(syncthing_local_discovery_summary *summary, guint offset)
     varint_length = tvb_get_varint(summary->tvb, offset, 10, &instance_id, ENC_VARINT_PROTOBUF);
     if (varint_length != 0)
     {
-        summary->instance_id_count += 1;
         proto_tree_add_int64(summary->tree, hf_syncthing_local_instance_id, summary->tvb, offset, varint_length, instance_id);
 
         return varint_length;
@@ -261,10 +134,131 @@ dissect_instance_id(syncthing_local_discovery_summary *summary, guint offset)
     }
 }
 
+static gint
+dissect_protobuf_field(
+    const syncthing_local_discovery_summary * summary,
+    const guint start_offset,
+    const syncthing_protobuf_field_definition *definitions,
+    const int defcount)
+{
+    gint varint_length;
+    guint64 key;
+    guint offset = start_offset;
+	
+    varint_length = tvb_get_varint(summary->tvb, offset, 4, &key, ENC_VARINT_PROTOBUF);
+    if (varint_length != 0) {
+        offset += varint_length;
+
+        const int tag = key >> 3;
+        int wire_type = key & 0x07;
+
+        for (int i = 0; i < defcount; ++i) {
+            syncthing_protobuf_field_definition *def = &definitions[i];
+
+            if (tag == def->tag) {
+                if (wire_type == def->wire_type) {
+                    // TODO: This is a lot of ifs. Can I make it flatter?
+
+                    proto_item *header = proto_tree_add_item(
+                        summary->tree,
+                        hf_syncthing_protobuf_entry,
+                        summary->tvb,
+                        start_offset,
+                        0, // don't know yet
+                        ENC_NA
+                    );
+                    proto_tree *subtree = proto_item_add_subtree(header, def->ett);
+
+                    syncthing_local_discovery_summary subsummary = { summary->tvb, summary->pinfo, subtree };
+                    gint result = def->handler(&subsummary, header, offset);
+
+                    proto_item_set_len(header, varint_length + result);
+                    return varint_length + result;
+                }
+                else {
+                    // TODO: Add expert info
+                    return -1;
+                }
+            }
+        }
+    }
+
+    // TODO: Invalid varint. Add expert info
+    return -1;
+}
+
+static gint
+dissect_next_field(syncthing_local_discovery_summary *summary, guint offset)
+{
+    /*
+    * The format is defined as follows:
+    * message Announce {
+    *   bytes           id          = 1;
+    *   repeated string addresses   = 2;
+    *   int64           instance_id = 3;
+    * }
+    */
+
+    // TODO: This should be initialized only once, maybe in the proto_register_syncthing?
+    syncthing_protobuf_field_definition field_definitions[] = {
+        /* { tag, wire_type, handler, ett, label } */
+        { 1, 2, &dissect_machine_id, ett_syncthing_local_machine_id, "Machine ID" },
+        { 2, 2, &dissect_address, ett_syncthing_local_address, "Sync Service Address" },
+        { 3, 0, &dissect_instance_id, ett_syncthing_local_instance_id, "Instance ID" }
+    };
+
+    return dissect_protobuf_field(summary, offset, field_definitions, sizeof(field_definitions));
+}
+
+static int
+dissect_syncthing_local_discovery(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
+{
+    // The first four bytes are 0x2EA7D90B in networ.k (big endian) byte order.
+    if (tvb_bytes_exist(tvb, 0, 4)) {
+        guint32 magic = tvb_get_ntohl(tvb, 0);
+    
+        if (magic == 0x2EA7D90B) {
+            // Ok, this looks like a syncthing local discovery packet
+            col_set_str(pinfo->cinfo, COL_PROTOCOL, "Syncthing");
+            col_clear(pinfo->cinfo, COL_INFO);
+
+
+            proto_item *ti = proto_tree_add_item(tree, proto_syncthing_local_discovery, tvb, 0,-1, ENC_NA);
+            proto_tree *syncthing_tree = proto_item_add_subtree(ti, ett_syncthing_local);
+            proto_tree_add_item(syncthing_tree, hf_syncthing_local_magic, tvb, 0, 4, ENC_BIG_ENDIAN);
+
+            guint offset = 4;
+            syncthing_local_discovery_summary summary = { tvb, pinfo, syncthing_tree };
+
+            while (offset < tvb_reported_length(tvb)) {
+                gint data_used = dissect_next_field(&summary, offset);
+
+                if (data_used != -1) {
+                    offset += data_used;
+                }
+                else {
+                    // TODO: Handle this error
+                    return offset;
+                }
+            }
+
+            return tvb_captured_length(tvb);
+        }
+    }
+
+    return 0;
+}
+
 void
 proto_register_syncthing(void)
 {
     static hf_register_info hf[] = {
+        { &hf_syncthing_protobuf_entry,
+            { "Protobuf entry", "syncthing.protobuf.entry",
+            FT_BYTES, BASE_NONE,
+            NULL, 0x0,
+            NULL, HFILL }
+        },
         { &hf_syncthing_protobuf_key,
             { "Protobuf key", "syncthing.protobuf.key",
             FT_BYTES, BASE_NONE,
